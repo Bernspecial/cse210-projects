79e7003a7ca3a05bf0c568a8a7825db1f38cdb41
Bernspecial <michaelbernardolayemi@gmail.com>
2024-01-30 12:18:01 +0100

    week 04 pre-activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new Assignment("Michael Bernard", "Web & Computer Programming");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Michael Bernard", "Web & Computer Programming","7.3", "7-5");
+        Console.WriteLine(mathAssignment.GetHomeWorkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Michael Bernard", "Web & computer programming", "Introduction to programming");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
+
     }
 }
